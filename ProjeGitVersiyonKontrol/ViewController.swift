3b9c6db06006cae670bda616eead2bc8d58e4ddb
Hikmet IŞILDAK <hikmet.isildak@gmail.com>
2021-06-30 02:17:58 +0300

    Projenin ilk adımı tamamlandı.

--- ProjeGitVersiyonKontrol/ViewController.swift
+++ ProjeGitVersiyonKontrol/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var lblMesaj: UILabel!
+    var sayi : Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnTikla(_ sender: Any) {
+        sayi += 1
+        lblMesaj.text = "\(sayi) defa tıkladın"
+    }
+    
 }
 
